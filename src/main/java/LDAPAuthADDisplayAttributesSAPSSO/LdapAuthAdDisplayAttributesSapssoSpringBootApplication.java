18e0d505b9d56a31254a4778bfc3a923b02a72b8
Suresh J <1540@DOMAIN.APCFSS.IN>
2023-11-26 14:59:29 +0530

    Initial commit

---
+++ src/main/java/LDAPAuthADDisplayAttributesSAPSSO/LdapAuthAdDisplayAttributesSapssoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package LDAPAuthADDisplayAttributesSAPSSO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthAdDisplayAttributesSapssoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAuthAdDisplayAttributesSapssoSpringBootApplication.class, args);
+	}
+
+}
